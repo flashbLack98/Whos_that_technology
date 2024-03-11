fa09eb2427887cc7784a0da5a13b0447875838a7
Nimesh Palakuttige Fernando <84867482+flashbLack98@users.noreply.github.com>
2024-03-11 21:33:56 +0100

    center main div

--- whos-that-technology/src/App.tsx
+++ whos-that-technology/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <h1>Who's that tecnology</h1>
       
       <div className='container'>
-        <div className="d-flex my-5">
+        <div className="d-flex justify-content-center my-5">
            <Pokemon></Pokemon>
           <div className="square bg-primary">Technology</div>  
         </div>
